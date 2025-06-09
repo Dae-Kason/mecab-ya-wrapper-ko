0d7663034f70eb135b6181abff6eb425e033595c
Ubuntu <ubuntu@ip-172-31-50-24.ap-northeast-2.compute.internal>
2025-06-09 13:08:46 +0000

    fix: Fixed wrong dic path

--- mecab.js
+++ mecab.js
@@ -1,77 +1,97 @@
 var cp = require('child_process');
 var sq = require('shell-quote');
+var fs = require('fs');
+var path = require('path');
 
-var MECAB_LIB_PATH =
-    process.env.MECAB_LIB_PATH ?
-    process.env.MECAB_LIB_PATH :
-    __dirname + '/mecab';
+var MECAB_LIB_PATH = process.env.MECAB_LIB_PATH || path.join(__dirname, 'mecab');
+var DEFAULT_USER_DIC_PATH = null;
 
-var buildCommand = function (text) {
-    return 'LD_LIBRARY_PATH=' + MECAB_LIB_PATH + ' ' +
-        sq.quote(['echo', text]) + ' | ' + MECAB_LIB_PATH + '/bin/mecab';
+var buildCommand = function (text, userDicPath) {
+  var quotedText = sq.quote(['echo', text]);
+  var mecabBin = MECAB_LIB_PATH + '/bin/mecab';
+  var sysDic = MECAB_LIB_PATH + '/lib/mecab/dic/mecab-ko-dic';
+
+  var cmd = 'LD_LIBRARY_PATH=' + MECAB_LIB_PATH + ' ' + quotedText + ' | ' +
+            mecabBin + ' -d ' + sysDic;
+
+  if (userDicPath && fs.existsSync(userDicPath)) {
+    cmd += ' -u ' + userDicPath;
+  }
+
+  return cmd;
 };
 
-var execMecab = function (text, callback) {
-    cp.exec(buildCommand(text), function(err, result) {
-        if (err) { return callback(err); }
-        callback(err, result);
-    });    
+var execMecab = function (text, userDicPath, callback) {
+  var command = buildCommand(text, userDicPath || DEFAULT_USER_DIC_PATH);
+
+  cp.exec(command, function (err, result) {
+    if (err) { return callback(err); }
+    callback(null, result);
+  });
 };
 
 var parseFunctions = {
-    'pos': function (result, elems) {
-        result.push([elems[0]].concat(elems[1].split(',')[0]));
-        return result;
-    },
+  'pos': function (result, elems) {
+    result.push([elems[0]].concat(elems[1].split(',')[0]));
+    return result;
+  },
 
-    'morphs': function (result, elems) {
-        result.push(elems[0]);
-        return result;
-    },
+  'morphs': function (result, elems) {
+    result.push(elems[0]);
+    return result;
+  },
 
-    'nouns': function (result, elems) {
-        var tag = elems[1].split(',')[0];
-        
-        if (tag === 'NNG' || tag === 'NNP') {
-            result.push(elems[0]);
-        }
-
-        return result;
+  'nouns': function (result, elems) {
+    var tag = elems[1].split(',')[0];
+    if (tag === 'NNG' || tag === 'NNP') {
+      result.push(elems[0]);
     }
+    return result;
+  }
 };
 
-var parse = function (text, method, callback) {
-    execMecab(text, function (err, result) {
-        if (err) { return callback(err); }
+var parse = function (text, method, userDicPath, callback) {
+  execMecab(text, userDicPath, function (err, result) {
+    if (err) { return callback(err); }
 
-        result = result.split('\n').reduce(function(parsed, line) {
-            var elems = line.split('\t');
+    var lines = result.split('\n');
+    var parsed = [];
 
-            if (elems.length > 1) {
-                return parseFunctions[method](parsed, elems);
-            } else {
-                return parsed;
-            }
-        }, []);
+    for (var i = 0; i < lines.length; i++) {
+      var line = lines[i];
+      var elems = line.split('\t');
 
-        callback(err, result);
-    });
+      if (elems.length > 1 && parseFunctions[method]) {
+        parseFunctions[method](parsed, elems);
+      }
+    }
+
+    callback(null, parsed);
+  });
 };
 
-var pos = function (text, callback) {
-    parse(text, 'pos', callback);
+var pos = function (text, callback, userDicPath) {
+  parse(text, 'pos', userDicPath, callback);
 };
 
-var morphs = function (text, callback) {
-    parse(text, 'morphs', callback);
+var morphs = function (text, callback, userDicPath) {
+  parse(text, 'morphs', userDicPath, callback);
 };
 
-var nouns = function (text, callback) {
-    parse(text, 'nouns', callback);
+var nouns = function (text, callback, userDicPath) {
+  parse(text, 'nouns', userDicPath, callback);
+};
+
+var setDefaultUserDic = function (path) {
+  if (fs.existsSync(path)) {
+    DEFAULT_USER_DIC_PATH = path;
+  }
 };
 
 module.exports = {
-    pos: pos,
-    morphs: morphs,
-    nouns: nouns
+  pos: pos,
+  morphs: morphs,
+  nouns: nouns,
+  setDefaultUserDic: setDefaultUserDic
 };
+
